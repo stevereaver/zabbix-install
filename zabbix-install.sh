c22b4ea4b477e294b0d8a05a8f09e2f0b086b7b9
Stephen Bancroft <stevereaver@gmail.com>
2018-02-01 09:10:04 +1100

    Update zabbix-install.sh

--- zabbix-install.sh
+++ zabbix-install.sh
@@ -24,9 +24,9 @@ FQDN=`host $HOSTNAME |cut -d' ' -f1`
 
 groupadd -g 130 zabbix
 useradd -u 130 -g 130 -c "Zabbix User" -s "/usr/bin/ksh" -m zabbix
-cp zabbix.xml.dist /lib/svc/manifest/site
+cp zabbix.xml.dist /lib/svc/manifest/site/zabbix.xml
 mkdir /etc/zabbix
-sed -e 's/<HOSTNAME>/'$FQDN'/g' zabbix_agentd.dist > /etc/zabbix/zabbix_agentd.conf 
+sed -e 's/<HOSTNAME>/'$FQDN'/g' zabbix_agentd.conf.dist > /etc/zabbix/zabbix_agentd.conf 
 chown -R zabbix:zabbix /etc/zabbix
 mkdir /opt/zabbix
 cd /opt/zabbix
