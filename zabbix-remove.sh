562250ae535ec17ab16aa858dfec3b342707367d
Stephen Bancroft <stevereaver@gmail.com>
2018-02-01 08:50:28 +1100

    Create zabbix-remove.sh

---
+++ zabbix-remove.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+#
+# Name:         zabbix-remove.sh
+# Authour:      Stephen Bancroft
+# Date:         01/31/2018
+# 
+# Script to automate the removal of Zabbix agent on Solaris 11.3
+# Run as root or sudo run it
+#
+
+# Check if running as root
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+# We can't really delete an SMF on Solaris, so we will just disable it.
+svcadm disable zabbix_agent
+
+rm -rf /etc/zabbix
+rm -rf /opt/zabbix
+
+groupdel zabbix
+userdel zabbix
